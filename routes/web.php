add21cc2f272801a8109630947f46bb46dc727a3
lonac <barakamalonao@yahoo.com>
2017-08-14 10:57:37 +0300

    modified:   routes/web.php resources/views/posts/

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,18 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'PostController@index')->name('home');
+
+Route::resource('users', 'UserController');
+
+Route::resource('roles', 'RoleController');
+
+Route::resource('permissions', 'PermissionController');
+
+Route::resource('posts', 'PostController');
+
+//Product controller
+
+Route::resource('products','ProductController');
