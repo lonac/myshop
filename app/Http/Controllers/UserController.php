cc3e5e65a331da25d86e3681ac2853f443e54064
lonac <barakamalonao@yahoo.com>
2017-08-14 11:13:25 +0300

    modified usercontroller and its users on view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Auth;
+
+use App\User;
+
+//Importing laravel-permission models
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+//Enables us to output flash messaging
+use Session;
+
+
 class UserController extends Controller
 {
+    public function __construct() {
+        $this->middleware(['auth', 'isAdmin']); //isAdmin middleware lets only users with a //specific permission permission to access these resources
+    }
     /**
      * Display a listing of the resource.
      *
@@ -13,7 +28,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        //Get all users and pass it to the view
+        $users = User::all(); 
+        return view('users.index')->with('users', $users);
     }
 
     /**
@@ -23,7 +40,9 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        //Get all roles and pass it to the view
+        $roles = Role::get();
+        return view('users.create', ['roles'=>$roles]);
     }
 
     /**
@@ -34,7 +53,29 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //Validate name, email and password fields
+        $this->validate($request, [
+            'name'=>'required|max:120',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|min:6|confirmed'
+        ]);
+
+        $user = User::create($request->only('email', 'name', 'password')); //Retrieving only the email and password data
+
+        $roles = $request['roles']; //Retrieving the roles field
+    //Checking if a role was selected
+        if (isset($roles)) {
+
+            foreach ($roles as $role) {
+            $role_r = Role::where('id', '=', $role)->firstOrFail();            
+            $user->assignRole($role_r); //Assigning role to user
+            }
+        }        
+    //Redirect to the users.index view and display message
+        return redirect()->route('users.index')
+            ->with('flash_message',
+             'User successfully added.');
+
     }
 
     /**
@@ -45,7 +86,7 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        return redirect('users'); 
     }
 
     /**
@@ -56,7 +97,11 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::findOrFail($id); //Get user with specified id
+        $roles = Role::get(); //Get all roles
+
+        return view('users.edit', compact('user', 'roles')); //pass user and roles data to view
+
     }
 
     /**
@@ -68,7 +113,27 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+         $user = User::findOrFail($id); //Get role specified by id
+
+    //Validate name, email and password fields  
+        $this->validate($request, [
+            'name'=>'required|max:120',
+            'email'=>'required|email|unique:users,email,'.$id,
+            'password'=>'required|min:6|confirmed'
+        ]);
+        $input = $request->only(['name', 'email', 'password']); //Retreive the name, email and password fields
+        $roles = $request['roles']; //Retreive all roles
+        $user->fill($input)->save();
+
+        if (isset($roles)) {        
+            $user->roles()->sync($roles);  //If one or more role is selected associate user to roles          
+        }        
+        else {
+            $user->roles()->detach(); //If no role is selected remove exisiting role associated to a user
+        }
+        return redirect()->route('users.index')
+            ->with('flash_message',
+             'User successfully edited.');
     }
 
     /**
@@ -79,6 +144,13 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+       //Find a user with a given id and delete
+        $user = User::findOrFail($id); 
+        $user->delete();
+
+        return redirect()->route('users.index')
+            ->with('flash_message',
+             'User successfully deleted.');
+    }
     }
 }
