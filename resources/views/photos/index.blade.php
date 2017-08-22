2fda207fd039b6a4e73452f2f66d0ebe84e14c5c
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-08-22 14:58:08 +0300

    Fix display product photos

--- resources/views/photos/index.blade.php
+++ resources/views/photos/index.blade.php
@@ -8,12 +8,9 @@
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
                     <div class="panel-heading"><h3>Products</h3></div>
-                    @foreach ($photos as $photo)
-                      
-                    <img src="<?php echo asset("storage/$photo->filename")?>"></img>
-
-                        </div>
-                    @endforeach
+                        @foreach ($photos as $photo)
+                          <img src="{{ asset('storage/' . $photo->filename) }}" alt="">
+                        @endforeach
                     </div>
                 </div>
             </div>
