eb6e7acf1605e26b509d2008692100e4c5ae66d5
lonac <barakamalonao@yahoo.com>
2017-08-30 09:48:06 +0300

    modified views on AUTH

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-primary">
+            <div class="panel panel-info">
                 <div class="panel-heading">Open Account on KKOO</div>
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('register') }}">
