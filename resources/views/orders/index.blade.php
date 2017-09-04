f62d6a0c7aade56dc8b31f2d0c858ec9075f33b7
lonac <barakamalonao@yahoo.com>
2017-09-04 09:13:31 +0300

    modified:   resources/views/orders/index.blade.php

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -14,15 +14,20 @@
                 <div class="panel panel-info">
                     <div class="panel-heading"><h3>Orders Review</h3></div>
                         <div class="panel-body">
+                           @if($order!==null)
                             <h4>
-                                <label for="name">User Name : </label>  {{Auth::user()->name}}<br>
-                              
+                               {{-- <label for="name">User Name : </label>  {{Auth::user()->name}}<br>
+                                <label for="orderid">Order ID : </label>  {{$order->id}}<br>
+                                <label for="reference">Payment Reference : </label> {{$order->reference}} <br> --}}
                                 <label for="shipping">Shipping Address : </label>
                                 <a href="{{url('cart/'.$cart->id.'/shippingaddress')}}" class="btn btn-primary">Review Address</a><br>
                             </h4>
-                         
+                           @else
+                            <h3>You have already placed an order</h3>
+                             <p><h2>Make <a href="{{url('/cart/'.$cart->id.'/orders/create')}}" class="btn btn-primary"
+                                >PAYMENT NOW</a> OR <a href="{{url('/cart')}}" class="btn btn-warning">PAYMENT LATER</a> for this Order</h2></p>
      
-                   
+                           @endif
                         </div>
                 </div>
             </div>
