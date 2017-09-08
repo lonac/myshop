a59a6dc9c0795cb4024d5bc18a0253bf26129b27
lonac <barakamalonao@yahoo.com>
2017-09-08 22:26:01 +0300

    modified:   resources/views/contacts.blade.php

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -20,9 +20,13 @@
 
             <div class="col-md-9 col-md-offset-0">
                 <div class="panel panel-info">
-                <div class="panel-heading"><h4>KKOO Service Center</h4></div>
+                <div class="panel-heading"><h4>KKOOTZ Service Center</h4></div>
                     <div class="panel-body">
-                        <p>Get Us Through</p>
+                        <p>You can contact KKOOTZ through our Agents at:</p>
+                        <label>DAR ES SALAAM:</label><br>
+                        <label>DODOMA</label><br>
+                        <label>MOSHI</label><br>
+                        <label>MOROGORO</label><br>
                     </div>
                 </div>
             </div>
